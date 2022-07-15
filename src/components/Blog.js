769a4b45d0784be3eafd40f3c8f301ca757409e2
Mychecksdead <mert.koksal.026@gmail.com>
2022-07-15 21:01:10 +0300

    Forgotten files

---
+++ src/components/Blog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+
+class Blog extends Component {
+  render() {
+    return (
+      <div style={{
+        height: '100px'
+      }}>
+        
+      </div>
+    )
+  }
+}
+
+export default Blog;
---
+++ src/components/Docs.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+
+class Docs extends Component {
+  render() {
+    return (
+      <div style={{
+        height: '100px'
+      }}>
+        
+      </div>
+    )
+  }
+}
+
+export default Docs;
