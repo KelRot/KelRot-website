7a95ac85999178cad2752d671f772092974cfe53
brnszt <74060122+brnszt@users.noreply.github.com>
2022-07-20 19:15:36 +0300

    css change

--- src/components/FRC.js
+++ src/components/FRC.js
@@ -17,7 +17,7 @@ let imgStyle = {
 
 const pStyle = {
   width: '440px',
-  color: 'white',
+  color: '#ffffff',
   overflow: 'hidden',
   margin: '30px 0 0 30px',
   float: 'left',
@@ -51,8 +51,8 @@ class FRC extends Component {
     for(let i=0; i<6; ++i){
       this.divs.push(<div style={boxStyle} key={i}>
         <div style={pStyle}>
-          <h1 style={{textAlign: 'left'}}>{titles[i]}</h1>
-          <p style={{marginTop: '10px'}}>{contents[i]}</p>
+          <h1 style={{textAlign: 'left', fontWeight: '600'}}>{titles[i]}</h1>
+          <p style={{marginTop: '10px', color: '#d9d9d9'}}>{contents[i]}</p>
         </div>
         <div style={{width: '500px', float: 'right'}}><img src={require('../images/' + images[i])} style={imgStyle}></img></div>
       </div>);
