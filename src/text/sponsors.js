ec6f6c96ee3d0646507ea08950f53cdf502aebfa
Mert Koksal <85784378+Mychecksdead@users.noreply.github.com>
2022-09-04 22:18:48 +0300

    texts

---
+++ src/text/sponsors.js
@@ -0,0 +1,73 @@
+const besiktas = {
+    p: 'Beşiktaş Belediyesi',
+    t: 'Beşiktaş Belediyesine 2021-2022 Sezonunda bize Platin Sponsorluk kapsamında destek oldukları için başta Sayın Belediye Başkanımız Rıza Akpolat’a ve bize Beşiktaş Belediyesiyle görüşmemizi sağlayan Okul Müdürümüz Muharrem Bayrak’a çok teşekkür ederiz.',
+    l: 'https://www.besiktas.bel.tr/',
+    im: 'besiktas.png'
+};
+
+const iskader = {
+    p: 'İskader(İstanbul Kabataş Erkek Liseliler Derneği)',
+    t: 'Kabataş Erkek Lisesinin değerlerini yaşatmak ve mezunları arasındaki dayanışmayı, iletişimi güçlendirmek amacıyla kurulan İSKADER, aynı zamanda genç Kabataş’lıların her konuda yanında olmayı amaçlamaktadır. Her alanda başarıyı yakalayan, bulundukları her ortamda fark yaratan, yeniliklere öncü olan yeni Kabataş mezunlarını yetiştirmek için faaliyet gösteren okulumuz derneği İSKADER bize hem yeni atölyemizin açılmasında hem de sezon boyunca verdikleri sayısız desteklerle takımımızın Ana Sponsorluğunu yapmıştır. Bu yolda bize destek gösteren Sayın Haldun Haznedar ve Sayın Vakıf Başkanımız Mustafa Kemal başta olmak üzere tüm İSKADER ailesine teşekkür ederiz!',
+    l: 'https://iskader.org.tr/',
+    im: 'iskader.png'
+};
+
+const kelev = {
+    p: 'KELEV',
+    t: 'Kabataş Erkek Lisesi Eğitim Vakfı çağdaş eğitim ve öğretimin devamına destek olmak isteyenlerle birlikte, Atatürk ilke ve devrimlerine bağlı, cumhuriyetimizi ve demokratik değerlerimizi koruyacak nesillerin yetişmesine katkıda bulunacak iklimi yaratmak ve yaşatılmasına katkı sağlamak yönünde olan sayısız faaliyetlerini 2021-2022 yılında hem maddi hem manevi anlamda bize destek olarak sürdürmeye devam etmiştir. Bu yolda bize destek veren Sayın KELEV Yönetim Kurulu Başkanımız Ahmet Çağlar başta olmak üzere tüm KELEV ailesine teşekkür ederiz!',
+    l: 'https://www.kelev.org.tr/',
+    im: 'kelev.png'
+};
+
+const ininal = {
+    p: 'İninal',
+    t: '2012 yılında kurulan, 10 yıldan kısa bir sürede neredeyse her gencin kullandığı bir ödeme platformu olan ininal, hedef kitlesi olan gençlere daha fazla destek olabilmek için 2021-2022 sezonunda takımımızın altın sponsorluğunu yaptı. Bu yolda bize yol gösterici olan anası',
+    l: 'https://www.ininal.com/',
+    im: 'ininal.png'
+};
+
+const atifabi = {
+    p: 'Atıf Kocaarslan',
+    t: 'First Robotik Yarışmasının geride bıraktığımız 2021-2022 sezonunda her zaman arkamızda duran, desteğini bizden hiç esirgemeyen okulumuzun 12’ Mezunu Atıf Abimiz takımımızın gümüş sponsorluğunu üstlendi. Kendisine destekleri için çok teşekkür ederiz 🐍',
+    l: '',
+    im: 'atif kocaarslan.png'
+};
+
+const volt = {
+    p: 'Volt Elektrik Motorları',
+    t: '1966 yılında, bobinaj üretimi yapmak üzere kurulan Volt Motor, 1987 yılından itibaren monofaze elektrik motoru ve devamında da trifaze elektrik motoru üretimine başlamış ve elektrik motoru üretimi sektöründeki yerini almıştır. Bugün itibariyle Türkiye’nin en prestijli motor firmalarından biri haline gelen Volt Elektrik Motoları 2021-2022 sezonunda takımımızın bronz sponsorluğunu üstlendi. Bu yolda bize destek ve yol gösterici olan başta Elçin Hanım’a ve tüm Volt Elektrik Motorları ailesine teşekkür ederiz!',
+    l: 'https://voltmotor.com.tr/',
+    im: 'volt.png'
+};
+
+const rest = {
+    p: 'Rest',
+    t: 'Yapay zeka, blockchain, IOT, bulut bilişim gibi anahtar teknolojileri, şirketlerin kendi öz kaynaklarıyla yönetebilecekleri ve endüstri 4.0 standartlarında rekabet edebilecekleri dönüşüm projelerini hayata geçiren Rest Yazılım, 2022-2023 sezonuna hazırlanırken takımımızın bronz sponsorluğunu üstlendi. Bu yolda bize yol gösterici olan, aynı zamanda Rest Yazılımın kurucuları olan Gürkal Ateşsaçan ve Sedat Cengize teşekkür ederiz!',
+    l: 'https://www.restyazilim.com/',
+    im: 'rest.png'
+};
+
+const panfez = {
+    p: 'Panfez',
+    t: 'Takımımızın uzun süredir hizmet sponsorluğunu üstlenen Panfez bu 2021-2022 sezonunda da bizi yarı yolda bırakmadı! Robotumuzun boyama masrafını üstlenerek bize destek oldular.',
+    l: '',
+    im: 'panfez.png'
+};
+
+const arcelik = {
+    p: 'Arçelik Garage',
+    t: 'Arçelik Garage yüksek dijitalleştirme, ileri prototipleme ve çok katmanlı üretim altyapısıyla fikir ile test üretimi arasındaki süreci gün düzeyine indirerek ürünlerin pazara çıkış süresini kısaltma mottosuyla çalışan bir Arçelik kuruluşudur. Arçelik Garage bize robotumuzu imal ederken hem parça ve malzeme anlamında hem de teknik bilgi anlamında destek olarak takımımızın hizmet sponsorluğunu üstlendi. Bu yolda bize destek olan tüm Arçelik Garage ailesine teşekkür ederiz!',
+    l: 'https://www.arcelikglobal.com/tr/teknoloji/ar-ge/arcelik-garage/',
+    im: 'garage.png'
+};
+
+const caglar = {
+    p: '',
+    t: '',
+    l: '',
+    im: 'caglar ailesi.png'
+}
+
+const s = [besiktas, iskader, kelev, ininal, atifabi, volt, rest, panfez, arcelik, caglar];
+
+export default s;
