6ddf0c47c3b540cf615e2ae325dc9e7e53854302
Beren Sözat <74060122+brnszt@users.noreply.github.com>
2022-09-06 19:23:04 +0300

    Update and rename vizyonmisyon to vision-mission.js

---
+++ src/text/vision-mission.js
@@ -0,0 +1,7 @@
+const vision = "Teknoloji ve robotik alanında öncü, takım halinde çalışmanın gereklerine hakim ve sahip, duyarlı, özgüveni yüksek, çok yönlü bireyler yetiştirmek, birlikte çalışma ruhunu sahip olduğu bütün platformlarda yaymak ve aşılamak."
+
+const mission = "Türk gençlerinin robotik ve yazılımla entegrasyonunu hızlandırmak adına, FRC’de başarılı olduğumuz gibi farklı yarışmalarda da başarılı olmak ve farklı projeler geliştirmek."
+
+const s = [vision, mission];
+
+export default s;
