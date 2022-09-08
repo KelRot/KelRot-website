163a6100ee3d76a7f3a89077aa3b4bae45a2c3e1
Mert Koksal <85784378+Mychecksdead@users.noreply.github.com>
2022-09-08 19:41:23 +0300

    Add files via upload

---
+++ src/Styles.js
@@ -0,0 +1,90 @@
+export const palette = {
+    background: 'linear-gradient(#262626, #404040)',
+    navbar: '#404040',
+    mainback: '#212121',
+    boxback: '#323232',
+    lighther: '#404040',
+    pcolor: '#00ff00',
+    acolor: '#d9d9d9',
+    hcolor: '#ffffff',
+    dark: '#323232'
+};
+
+
+export const boxStyle = {
+    width: '1000px',
+    backgroundColor: palette.boxback,
+    margin: '40px',
+    borderRadius: '20px',
+    overflow: 'hidden',
+    color: palette.pcolor
+};
+
+export const aStyle = {
+    fontWeight: '400',
+    color: palette.acolor,
+    margin: '18px 20px 0 25px',
+    float: 'left',
+    fontSize: '25px',
+    float: 'left'
+};
+ 
+
+export const pStyle = {
+    color: palette.pcolor,
+    overflow: 'hidden',
+    margin: '30px 20px 0 30px',
+    float: 'left',
+    fontSize: '18px',
+    textAlign: 'justify',
+    paddingBottom: '30px'
+};
+
+export const miniBoxStyle = {
+    width: '200px',
+    overflow: 'hidden',
+    margin: '25px 0 0 40px',
+    float: 'left',
+    fontSize: '18px',
+    textAlign: 'center',
+    backgroundColor: palette.lighther,
+    padding: '15px 0'
+};
+  
+export const h2Style = {
+    color: '#d9d9d9', 
+    padding: '20px 0 0 20px',
+    fontWeight: '400'
+};
+  
+
+export const littleBoxStyle = {
+    width: '180px',
+    height: '130px',
+    margin: '30px 20px 20px 20px',
+    float: 'left',
+    backgroundColor: palette.lighter
+};
+  
+export const littlePStyle = {
+    overflow: 'auto',
+    backgroundColor: palette.pcolor,
+    float: 'bottom',
+    textAlign: 'center'
+};
+
+export const h1Style = {
+    color: palette.hcolor,
+    margin: '25px 0 0 20px',
+    float: 'left',
+    fontWeight: '600'
+};
+
+
+export const personBoxStyle = {
+    width: '150px',
+    height: '144px',
+    margin: '30px 25px 0 25px',
+    float: 'left',
+    backgroundColor: palette.background
+};
