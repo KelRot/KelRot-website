3d97bab69d0e39c375aef39281050d66c405cc9c
Beren Sözat <74060122+brnszt@users.noreply.github.com>
2022-09-13 23:53:58 +0300

    Update Styles.js

--- src/Styles.js
+++ src/Styles.js
@@ -3,13 +3,12 @@ export const palette = {
     navbar: '#404040',
     mainback: '#212121',
     boxback: '#323232',
-    lighther: '#404040',
-    pcolor: '#00ff00',
+    lighter: '#404040',
+    pcolor: '#d9d9d9',
     acolor: '#d9d9d9',
     hcolor: '#ffffff',
     dark: '#323232'
 };
-
 
 export const boxStyle = {
     width: '1000px',
@@ -22,22 +21,26 @@ export const boxStyle = {
 
 export const aStyle = {
     fontWeight: '400',
-    color: palette.acolor,
     margin: '18px 20px 0 25px',
     float: 'left',
     fontSize: '25px',
-    float: 'left'
+    width: '1000px'
 };
- 
 
 export const pStyle = {
     color: palette.pcolor,
-    overflow: 'hidden',
-    margin: '30px 20px 0 30px',
+    margin: '10px 8px 0 0',
     float: 'left',
     fontSize: '18px',
-    textAlign: 'justify',
     paddingBottom: '30px'
+};
+
+export const h1Style = {
+    color: palette.hcolor,
+    float: 'left',
+    fontWeight: '600',
+    fontSize: '36px',
+    textAlign: 'left'
 };
 
 export const miniBoxStyle = {
@@ -47,39 +50,26 @@ export const miniBoxStyle = {
     float: 'left',
     fontSize: '18px',
     textAlign: 'center',
-    backgroundColor: palette.lighther,
+    backgroundColor: palette.lighter,
     padding: '15px 0'
 };
   
 export const h2Style = {
     color: '#d9d9d9', 
     padding: '20px 0 0 20px',
-    fontWeight: '400'
+    fontWeight: '400',
+    fontSize: '26px',
+    width: '1000px',
+    float: 'left'
 };
-  
 
 export const littleBoxStyle = {
     width: '180px',
     height: '130px',
-    margin: '30px 20px 20px 20px',
+    margin: '30px 20px',
     float: 'left',
-    backgroundColor: palette.lighter
+    backgroundColor: '#437624'
 };
-  
-export const littlePStyle = {
-    overflow: 'auto',
-    backgroundColor: palette.pcolor,
-    float: 'bottom',
-    textAlign: 'center'
-};
-
-export const h1Style = {
-    color: palette.hcolor,
-    margin: '25px 0 0 20px',
-    float: 'left',
-    fontWeight: '600'
-};
-
 
 export const personBoxStyle = {
     width: '150px',
@@ -90,5 +80,5 @@ export const personBoxStyle = {
 };
 
 export const imgStyle = {
-
+    maxWidth: '500px'
 };
